a0ed930b0509005d086a6d4b685aa531cc8f00a1
AndreiLS <leganas@gmail.com>
2018-03-24 23:19:30 +0300

    v 1.2 - Правики JUnit тестов

--- src/test/java/NetEngineTest.java
+++ src/test/java/NetEngineTest.java
@@ -1,7 +1,12 @@
 import by.legan.library.netengine.controller.NetClientController;
 import by.legan.library.netengine.controller.NetServerController;
 import by.legan.library.netengine.network.client.AbstractClient;
-import org.junit.*;
+import by.legan.library.netengine.network.packeges.clientTOserver.ClientMessage;
+import by.legan.library.netengine.network.packeges.serverTOclient.ServerMessage;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
 import org.junit.runners.MethodSorters;
 
 import java.net.InetAddress;
@@ -11,7 +16,7 @@ import static junit.framework.TestCase.assertTrue;
 import static org.junit.Assert.assertEquals;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
-public class ControllerConnectionTest {
+public class NetEngineTest {
 
     static InetAddress inet;
     static InetAddress[] ips;
@@ -37,8 +42,9 @@ public class ControllerConnectionTest {
     static NetServerController netServerController;
     static NetClientController netClientController;
 
+    // Проверка конекта
     @Test(timeout = 10000)
-    public void controller_Interaction_test() {
+    public void a_controller_Interaction_test() {
 
         System.out.println("TEST : Start server");
         netServerController = new NetServerController("Server");
@@ -62,6 +68,27 @@ public class ControllerConnectionTest {
         assertEquals(netClientController.client.getStatus(), AbstractClient.NetClientStatus.online);
         System.out.println("TEST : Connect client server successful");
 
+    }
+
+    @Test(timeout = 10000)
+    public void b_client_message_send_to_server_test(){
+        System.out.println("TEST : Client_Message send to server test");
+        ClientMessage.RequestServerInfo msg = new ClientMessage.RequestServerInfo();
+        netClientController.addClientMessageToQuery(msg);
+        final Boolean[] flag = {false};
+        netClientController.setGuiListener(message -> {
+            ServerMessage.ReturnServerInfo response = (ServerMessage.ReturnServerInfo) message;
+            System.out.println("TEST : Response name sender : " + response.getName());
+            flag[0] = true;
+        });
+        do {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        } while (flag[0] != true);
+        assertEquals(flag[0], true);
     }
 
     @AfterClass
